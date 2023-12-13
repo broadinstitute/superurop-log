0036b8cf22090186fbb7e49e37e59dd38cc7c43a
Eddy Ogola <eddy.ogola1@gmail.com>
2023-12-13 12:08:19 -0500

    freeze backbone and add training boilerplate

--- experiments/fcos/fcos.py
+++ experiments/fcos/fcos.py
@@ -2,6 +2,9 @@ import torchvision
 import torch
 import cv2
 
+LR = 1e-4
+EPOCHS = 20
+
 # load vanilla(non-pretrained) FCOS model from torchvision
 model = torchvision.models.detection.fcos_resnet50_fpn()
 model.eval()
@@ -21,3 +24,30 @@ out = out[0]
 bounding_boxes, scores = out["boxes"], out["scores"]
 
 print(list(zip(bounding_boxes, scores)))
+
+
+# freeze entire model first
+for param in model.parameters():
+    param.requires_grad = False
+
+# or just freeze the resnet50 backbone
+# for param in model.backbone.parameters():
+#     param.requires_grad = False
+
+# unfreeze regression head, and final transform module
+for param in model.head.regression_head.parameters():
+    param.requires_grad = True
+for param in model.transform.parameters():
+    param.requires_grad = True
+
+# training loop boilerplate
+optimizer = torch.optim.Adam(filter(lambda p: p.requires_grad, model.parameters()), lr=LR)
+
+# dataloader = # define dataloader
+
+for i, epoch in range(EPOCHS):
+    for images, targets in dataloader:
+        optimizer.zero_grad()
+        loss = torch.nn.CrossEntropyLoss()
+        loss.backward()
+        optimizer.step()
